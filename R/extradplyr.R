a3f69434ac74e9d93e6a7c35b7051eb74a80ef46
DuncanMcKinnon <duncanmckinnon96@gmail.com>
2020-05-28 17:07:46 -0700

    Cleaning up functional logic, adding documentation

--- R/extradplyr.R
+++ R/extradplyr.R
@@ -1,30 +1,46 @@
 
-# all purpose function to generate summary statistics for a given attribute grouped by a target attribute
-attribute_stats <- function(data, summary_attr, group_attr = NULL){
-  assertthat::assert_that(is.data.frame(data))
-  assertthat::assert_that(is.string(summary_attr))
-  summary_attr_ <- as.name(summary_attr)
+#' generate summary for a given attribute grouped by a target attribute
+#'
+#' @param data a data frame or tibble type object
+#' @param summary_attr a column label as a string to summarize
+#' @param group_attr a column label to group by before summarizing (optional)
+#' @returns a grouped summary table for the attribute
+attribute.summary <- function(data, summary_attr, group_attr = NULL){
   if(is.null(group_attr)) {
-    response <- data %>%
-      dplyr::select_(summary_attr_)
+    response <- suppressWarnings(dplyr::select_(data, summary_attr))
   } else {
-    assertthat::assert_that(is.string(group_attr))
-    group_attr_ <- as.name(group_attr)
-    response <- data %>%
-      dplyr::select_(summary_attr_, group_attr_) %>%
-      dplyr::group_by_(group_attr_)
+    response <- suppressWarnings(dplyr::group_by_(dplyr::select_(data, summary_attr, group_attr), group_attr) )
   }
-  return(
-    response %>%
-        dplyr::summarise_(
-          min = interp(~min(var, na.rm = T), var = summary_attr_),
-          p25 = interp(~quantile(var, probs=0.25, na.rm = T), var = summary_attr_),
-          mean = interp(~mean(var, na.rm = T), var = summary_attr_),
-          median = interp(~median(var, na.rm = T), var = summary_attr_),
-          p75 = interp(~quantile(var, probs=0.75, na.rm = T), var = summary_attr_),
-          max = interp(~max(var, na.rm = T), var = summary_attr_),
-          sd = interp(~sd(var, na.rm = T), var = summary_attr_)
-        )
-  )
+  return( attribute.stats(response, summary_attr) )
 }
 
+#' get the summary statistics for an attribute (grouped or otherwise)
+#'
+#' @param data a data frame or tibble type object
+#' @param stats_attr a column label as a string to get stats for
+#' @returns a tibble with the summary stats for the specified column/grouping
+attribute.stats <- function(data, stats_attr){
+  attribute_types <- attribute.types(data)
+  stats_attr_ <- as.name(stats_attr)
+  if( attribute_types[stats_attr] %in% c('integer', 'numeric', 'complex', 'double') ){
+    return( suppressWarnings(
+      dplyr::summarise_( data,
+                         min = lazyeval::interp(~min(var, na.rm = T), var = stats_attr_),
+                         p25 = lazyeval::interp(~quantile(var, probs=0.25, na.rm = T), var = stats_attr_),
+                         mean = lazyeval::interp(~mean(var, na.rm = T), var = stats_attr_),
+                         median = lazyeval::interp(~median(var, na.rm = T), var = stats_attr_),
+                         p75 = lazyeval::interp(~quantile(var, probs=0.75, na.rm = T), var = stats_attr_),
+                         max = lazyeval::interp(~max(var, na.rm = T), var = stats_attr_),
+                         sd = lazyeval::interp(~sd(var, na.rm = T), var = stats_attr_))))
+  } else if( attribute_types[stats_attr] %in% c('factor', 'logical', 'character') ){
+    return( suppressWarnings(dplyr::count_( data, stats_attr_)))
+  }
+}
+
+#' get the type of attribute in each column of the data, as a vector
+#'
+#' @param data a data frame or tibble type object
+#' @returns a character vector of the class type of each column
+attribute.types <- function(data){
+  sapply(data, class, USE.NAMES = T)
+}
